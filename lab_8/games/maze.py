83dda8229cb547df9805663152c56c1c2ac72fc2
tsda04 <tsda04@mail.ru>
2024-12-07 02:06:48 +0400

    обертка класса: повышение читаемости кода и его организации

--- lab_8/games/maze.py
+++ lab_8/games/maze.py
@@ -1,47 +1,61 @@
-def maze():
-    maze_layout = [
-        "#########",
-        "#       #",
-        "# ##### #",
-        "# #   # #",
-        "# # # # #",
-        "#   #   #",
-        "#########"
-    ]
+class MazeGame:
+    def __init__(self):
+        self.maze_layout = [
+            "#########",
+            "#       #",
+            "# ##### #",
+            "# #   # #",
+            "# # # # #",
+            "#   #   #",
+            "#########",
+        ]
+        self.player_pos = [1, 1]  # Начальная позиция игрока
+        self.exit_pos = [5, 7]  # Позиция выхода
 
-    player_pos = [1, 1]  # Начальная позиция игрока
-    exit_pos = [5, 7]    # Позиция выхода
-
-    def print_maze():
-        for i in range(len(maze_layout)):
-            row = list(maze_layout[i])
-            if player_pos[0] == i:
-                row[player_pos[1]] = 'P'  # Отображаем игрока
+    def print_maze(self):
+        """Отображение лабиринта с игроком."""
+        for i in range(len(self.maze_layout)):
+            row = list(self.maze_layout[i])
+            if self.player_pos[0] == i:
+                row[self.player_pos[1]] = "P"  # Отображаем игрока
             print("".join(row))
 
-    while True:
-        print_maze()
+    def is_move_valid(self, new_pos):
+        """Проверка, можно ли переместиться на новую позицию."""
+        return self.maze_layout[new_pos[0]][new_pos[1]] == " "
 
-        if player_pos == exit_pos:
-            print("Поздравляю! Вы нашли выход!")
-            break
+    def play(self):
+        """Основной игровой процесс."""
+        while True:
+            self.print_maze()
 
-        move = input("Введите ваше движение (W - вверх, S - вниз, A - влево, D - вправо): ").lower()
+            if self.player_pos == self.exit_pos:
+                print("Поздравляю! Вы нашли выход!")
+                break
 
-        if move == "w":  # Вверх
-            new_pos = [player_pos[0] - 1, player_pos[1]]
-        elif move == "s":  # Вниз
-            new_pos = [player_pos[0] + 1, player_pos[1]]
-        elif move == "a":  # Влево
-            new_pos = [player_pos[0], player_pos[1] - 1]
-        elif move == "d":  # Вправо
-            new_pos = [player_pos[0], player_pos[1] + 1]
-        else:
-            print("Неверное движение! Попробуйте снова.")
-            continue
+            move = input(
+                "Введите ваше движение (W - вверх, S - вниз, A - влево, D - вправо): "
+            ).lower()
 
-        # Проверяем, можно ли переместиться на новую позицию
-        if maze_layout[new_pos[0]][new_pos[1]] == ' ':
-            player_pos = new_pos
-        else:
-            print("Вы не можете пройти через стену!")
+            if move == "w":  # Вверх
+                new_pos = [self.player_pos[0] - 1, self.player_pos[1]]
+            elif move == "s":  # Вниз
+                new_pos = [self.player_pos[0] + 1, self.player_pos[1]]
+            elif move == "a":  # Влево
+                new_pos = [self.player_pos[0], self.player_pos[1] - 1]
+            elif move == "d":  # Вправо
+                new_pos = [self.player_pos[0], self.player_pos[1] + 1]
+            else:
+                print("Неверное движение! Попробуйте снова.")
+                continue
+
+            # Проверяем, можно ли переместиться на новую позицию
+            if self.is_move_valid(new_pos):
+                self.player_pos = new_pos
+            else:
+                print("Вы не можете пройти через стену!")
+
+
+def maze():
+    game = MazeGame()
+    game.play()
