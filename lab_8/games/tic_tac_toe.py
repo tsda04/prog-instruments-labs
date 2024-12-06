c44454319664ff1adfb6d50c32474c10ca6764d8
tsda04 <tsda04@mail.ru>
2024-12-07 02:07:20 +0400

    обертка класса: повышение читаемости кода и его организации

--- lab_8/games/tic_tac_toe.py
+++ lab_8/games/tic_tac_toe.py
@@ -1,41 +1,58 @@
-def tic_tac_toe():
-    board = [' ' for _ in range(9)]  # Инициализация пустого поля
+class TicTacToe:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]  # Инициализация пустого поля
+        self.current_player = "X"  # Начальный игрок
 
-    def print_board():
-        print(f"{board[0]} | {board[1]} | {board[2]}")
+    def print_board(self):
+        """Отображение текущего состояния игрового поля."""
+        print(f"{self.board[0]} | {self.board[1]} | {self.board[2]}")
         print("--+---+--")
-        print(f"{board[3]} | {board[4]} | {board[5]}")
+        print(f"{self.board[3]} | {self.board[4]} | {self.board[5]}")
         print("--+---+--")
-        print(f"{board[6]} | {board[7]} | {board[8]}")
+        print(f"{self.board[6]} | {self.board[7]} | {self.board[8]}")
 
-    def check_winner():
+    def check_winner(self):
+        """Проверка на наличие победителя."""
         winning_combinations = [
-            (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Горизонтали
-            (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Вертикали
-            (0, 4, 8), (2, 4, 6)               # Диагонали
+            (0, 1, 2),
+            (3, 4, 5),
+            (6, 7, 8),  # Горизонтали
+            (0, 3, 6),
+            (1, 4, 7),
+            (2, 5, 8),  # Вертикали
+            (0, 4, 8),
+            (2, 4, 6),  # Диагонали
         ]
         for combo in winning_combinations:
-            if board[combo[0]] == board[combo[1]] == board[combo[2]] != ' ':
-                return board[combo[0]]
+            if (
+                self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != " "
+            ):
+                return self.board[combo[0]]
         return None
 
-    current_player = 'X'
-    for turn in range(9):
-        print_board()
-        move = int(input(f"Игрок {current_player}, выберите позицию (1-9): ")) - 1
+    def play(self):
+        """Основной игровой процесс."""
+        for turn in range(9):
+            self.print_board()
+            move = int(input(f"Игрок {self.current_player}, выберите позицию (1-9): ")) - 1
 
-        if move < 0 or move > 8 or board[move] != ' ':
-            print("Некорректный ввод или позиция уже занята. Попробуйте снова.")
-            continue
+            if move < 0 or move > 8 or self.board[move] != " ":
+                print("Некорректный ввод или позиция уже занята. Попробуйте снова.")
+                continue
 
-        board[move] = current_player
-        winner = check_winner()
-        if winner:
-            print_board()
-            print(f"Поздравляем! Игрок {winner} выиграл!")
-            return
+            self.board[move] = self.current_player
+            winner = self.check_winner()
+            if winner:
+                self.print_board()
+                print(f"Поздравляем! Игрок {winner} выиграл!")
+                return
 
-        current_player = 'O' if current_player == 'X' else 'X'
+            self.current_player = "O" if self.current_player == "X" else "X"
 
-    print_board()
-    print("Игра закончилась вничью!")
+        self.print_board()
+        print("Игра закончилась вничью!")
+
+
+def tic_tac_toe():
+    game = TicTacToe()
+    game.play()
