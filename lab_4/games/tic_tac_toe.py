8d83033d3a799115beb46abef6e2d400d00d3a71
tsda04 <tsda04@mail.ru>
2024-12-07 13:45:07 +0400

    add: logging в крестики-нолики

--- lab_4/games/tic_tac_toe.py
+++ lab_4/games/tic_tac_toe.py
@@ -1,4 +1,9 @@
+import logging
 from typing import Optional, List
+
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class TicTacToe:
@@ -8,6 +13,7 @@ class TicTacToe:
         """Инициализация пустого поля и текущего игрока."""
         self.board: List[str] = [" " for _ in range(9)]  # Инициализация пустого поля
         self.current_player: str = "X"  # Начальный игрок
+        logging.info("Игра в Крестики-Нолики начата. Начальный игрок: %s", self.current_player)
 
     def print_board(self) -> None:
         """Отображение текущего состояния игрового поля."""
@@ -31,6 +37,7 @@ class TicTacToe:
         ]
         for combo in winning_combinations:
             if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != " ":
+                logging.info("Игрок %s выиграл!", self.board[combo[0]])
                 return self.board[combo[0]]
         return None
 
@@ -43,15 +50,19 @@ class TicTacToe:
             # Проверка на корректность ввода
             if not move.isdigit() or not (1 <= int(move) <= 9):
                 print("Некорректный ввод. Пожалуйста, введите число от 1 до 9.")
+                logging.warning("Игрок %s ввел некорректное значение: %s", self.current_player, move)
                 continue
 
             move = int(move) - 1
 
             if self.board[move] != " ":
                 print("Позиция уже занята. Попробуйте снова.")
+                logging.warning("Игрок %s пытается занять уже занятую позицию: %d", self.current_player, move + 1)
                 continue
 
             self.board[move] = self.current_player
+            logging.info("Игрок %s поставил знак на позицию %d", self.current_player, move + 1)
+
             winner = self.check_winner()
             if winner:
                 self.print_board()
@@ -62,6 +73,7 @@ class TicTacToe:
 
         self.print_board()
         print("Игра закончилась вничью!")
+        logging.info("Игра закончилась вничью.")
 
 
 def tic_tac_toe() -> None:
